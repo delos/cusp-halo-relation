minor fix for proper broadcast in cuspNFW

cusp_halo_relation/cuspNFW.py
@@ -26,8 +26,7 @@ def mass(r,rs,rhos,A):
   
   This profile only makes sense if rhos * rs**1.5 >= A.
   '''
-  x = r/rs
-  y = A / (rhos * rs**1.5) # <= 1
+  x,y = np.broadcast_arrays(r/rs,A/(rhos * rs**1.5))
   return 4*np.pi*rs**3*rhos*np.piecewise(y,[y<0.001,y>0.999],[__mass_smallA,__mass_largeA,__mass],x)
 
 def r2_from_rs(rs,rhos,A):