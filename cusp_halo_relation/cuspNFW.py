9789b728111454c6e6f93f2d7bfb4f210956aac1
Sten Delos <sten@mpa-garching.mpg.de>
2025-05-07 12:22:27 -0700

    cuspNFW: handle very small minimum c

--- cusp_halo_relation/cuspNFW.py
+++ cusp_halo_relation/cuspNFW.py
@@ -55,8 +55,13 @@ def R_from_M(M,rho_vir):
 def __rhos_from_c_NFW(c,rho_vir):
   return c**3/(np.log(1+c)-c/(1+c))*rho_vir/3.
 
-def __min_params(c):
+def __min_params_largec(c):
   return 384*np.pi*(np.sqrt(c/(2 + c)) - np.arcsinh(np.sqrt(c/2)))**2/c**3
+def __min_params_smallc(c):
+  return 16.755160819145562 - 15.079644737231007*c + 10.12490432356939*c**2
+def __min_params(c):
+  return np.piecewise(c,[c<0.001],[__min_params_smallc,__min_params_largec])
+
 
 def scale_from_c(c,M,A,rho_vir,cmin_error=True):
   '''
@@ -120,4 +125,5 @@ def c_min(M,A,rho_vir):
   Minimum sensible concentration c given halo mass M and cusp amplitude A.
   Here rho_vir is the virial density.
   '''
-  return np.exp(root_scalar(lambda lnc: np.log(__min_params(np.exp(lnc))*A**2/(M*rho_vir)),x0=0.,x1=1.).root)
+  lnc = root_scalar(lambda lnc: np.log(__min_params(np.exp(lnc))*A**2/(M*rho_vir)),x0=0.,x1=1.).root
+  return np.exp(lnc)
