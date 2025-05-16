147e075535551396e279c3bd5c8709d93fae4536
Sten Delos <sten@mpa-garching.mpg.de>
2025-05-16 16:47:32 -0700

    add example scripts

--- cusp_halo_relation/cuspNFW.py
+++ cusp_halo_relation/cuspNFW.py
@@ -2,16 +2,16 @@ import numpy as np
 from scipy.optimize import root_scalar
 from scipy.integrate import cumtrapz
 
-def density(r,rs,rhos,A):
+def density(r,r_s,rho_s,A):
   '''
   Evaluate density at radius r for a cusp-NFW density profile with scale radius
-  rs, scale density rhos, and cusp amplitude A.
+  r_s, scale density rho_s, and cusp amplitude A.
   
-  This profile only makes sense if rhos * rs**1.5 >= A.
+  This profile only makes sense if rho_s * r_s**1.5 >= A.
   '''
-  x = r/rs
-  y = A / (rhos * rs**1.5) # <= 1
-  return np.sqrt(x+y**2)/(x**1.5*(1+x)**2) * rhos
+  x = r/r_s
+  y = A / (rho_s * r_s**1.5) # <= 1
+  return np.sqrt(x+y**2)/(x**1.5*(1+x)**2) * rho_s
 
 def __mass(y,x):
   return 2*np.arcsinh(np.sqrt(x)/y) - (2 - y**2)*np.arctanh(np.sqrt(x*(1 - y**2)/(x + y**2)))/np.sqrt(1 - y**2) - np.sqrt(x*(x + y**2))/(1 + x)
@@ -20,34 +20,34 @@ def __mass_smallA(y,x):
 def __mass_largeA(y,x):
   return np.sqrt(x)*(-30 - 10*x*(7 - y) + x**2*(-37 + y*(4 + 3*y)))/(15.*(1 + x)**2.5) + 2*np.arcsinh(np.sqrt(x))
 
-def mass(r,rs,rhos,A):
+def mass(r,r_s,rho_s,A):
   '''
   Evaluate mass enclosed within radius r for a cusp-NFW density profile with
-  scale radius rs, scale density rhos, and cusp amplitude A.
+  scale radius r_s, scale density rho_s, and cusp amplitude A.
   
-  This profile only makes sense if rhos * rs**1.5 >= A.
+  This profile only makes sense if rho_s * r_s**1.5 >= A.
   '''
-  x,y = np.broadcast_arrays(r/rs,A/(rhos * rs**1.5))
-  return 4*np.pi*rs**3*rhos*np.piecewise(y,[(0.<=y)&(y<0.001),(0.001<=y)&(y<=0.999),(0.999<y)&(y<1.001)],[__mass_smallA,__mass,__mass_largeA,np.nan],x)
+  x,y = np.broadcast_arrays(r/r_s,A/(rho_s * r_s**1.5))
+  return 4*np.pi*r_s**3*rho_s*np.piecewise(y,[(0.<=y)&(y<0.001),(0.001<=y)&(y<=0.999),(0.999<y)&(y<1.001)],[__mass_smallA,__mass,__mass_largeA,np.nan],x)
 
-def r2_from_rs(rs,rhos,A):
+def r2_from_rs(r_s,rho_s,A):
   '''
   Find the radius r_{-2} at which dlog(rho)/dlog(r) = -2 for a cusp-NFW density
-  profile with scale radius rs, scale density rhos, and cusp amplitude A.
+  profile with scale radius r_s, scale density rho_s, and cusp amplitude A.
   
-  r2 ranges from rs/2 (if A=rhos*rs**1.5) to rs (if A=0).
+  r_{-2} ranges from r_s/2 (if A=rho_s*r_s**1.5) to r_s (if A=0).
   '''
-  y = A / (rhos * rs**1.5) # <= 1
-  return (1/2. - 3*y**2/4. + np.sqrt(4 - 4*y**2 + 9*y**4)/4.) * rs
+  y = A / (rho_s * r_s**1.5) # <= 1
+  return 0.5 * (1. - 1.5*y**2 + np.sqrt(1.-y**2+2.25*y**4)) * r_s
 
-def rs_from_r2(r2,rhos,A):
+def rs_from_r2(r_2,rho_s,A):
   '''
-  Find the cusp-NFW scale radius rs, given r2 (the radius r_{-2} at which
-  dlog(rho)/dlog(r) = -2), the scale density rhos, and the cusp amplitude A.
+  Find the cusp-NFW scale radius r_s, given r_2 (the radius r_{-2} at which
+  dlog(rho)/dlog(r) = -2), the scale density rho_s, and the cusp amplitude A.
   '''
-  z = A / (rhos * r2**1.5) # <= 2**1.5
-  f = (8 + 6*z*(24*z + np.sqrt(72 + 564*z**2 + 6*z**4)))**(1./3)
-  return (4 + f*(2 + f) - 6*z**2)/(6.*f) * r2
+  z = A / (rho_s * r_2**1.5) # <= 2**1.5
+  f = (1. + 18.*z**2 + 0.75*z*np.sqrt(72 + 564*z**2 + 6*z**4))**(1./3)
+  return ((1/3.-z**2/2.)/f + (1+f)/3.) * r_2
   
 def R_from_M(M,rho_vir):
   '''Virial radius R given halo mass M and virial density rho_vir.'''
@@ -83,29 +83,29 @@ def scale_from_c(c,M,A,rho_vir,cmin_error=True):
     
     cmin_error: boolean
       If True, we raise an exception if the concentration is too low and would
-      violate rhos * rs**1.5 >= A. If False, we increase the concentration to
+      violate rho_s * r_s**1.5 >= A. If False, we increase the concentration to
       its minimum value and continue. Default is True.
     
   Returns:
     
-    rs, rhos: floats
+    r_s, rho_s: floats
       The scale radius and scale density, respectively.
   
   '''
   R = R_from_M(M,rho_vir)
-  r2 = R / c # r_{-2}
+  r_2 = R / c # r_{-2}
   if A == 0.:
-    return r2,__rhos_from_c_NFW(c,rho_vir)
+    return r_2,__rhos_from_c_NFW(c,rho_vir)
   if M*rho_vir/A**2 < __min_params(c):
     if cmin_error:
       raise Exception('M*rho_vir/A**2=%.3e must be >%.3e for c=%.2f.'%(M*rho_vir/A**2,__min_params(c),c))
     else:
       c = c_min(M,A,rho_vir)
-      r2 = R / c # r_{-2}
-  rhos = root_scalar(lambda rhos: np.log(mass(R,rs_from_r2(r2,rhos,A),rhos,A)/M),
-                     bracket=[A/(2*r2)**1.5,__rhos_from_c_NFW(c,rho_vir)]).root
-  rs = rs_from_r2(r2,rhos,A)
-  return rs, rhos
+      r_2 = R / c # r_{-2}
+  rho_s = root_scalar(lambda rho_s: np.log(mass(R,rs_from_r2(r_2,rho_s,A),rho_s,A)/M),
+                     bracket=[A/(2*r_2)**1.5,__rhos_from_c_NFW(c,rho_vir)]).root
+  r_s = rs_from_r2(r_2,rho_s,A)
+  return r_s, rho_s
 
 def A_max(c,M,rho_vir):
   '''
@@ -136,23 +136,23 @@ def __sigma2_r_large(x):
   logx = np.log(x)
   return (-3./16+logx/4)/x + (69./200+logx/10)/x**2 + (-97./1200-logx/20)/x**3 + (71./3675+logx/35)/x**4 + (-1./3136-logx/56)/x**5 + (-1271./211680+logx/84)/x**6
 
-def sigma_r(r,rs,rhos,A,G=1.):
+def sigma_r(r,r_s,rho_s,A,G=1.):
   '''
   Evaluate radial velocity dispersion at radius r for a cusp-NFW density
-  profile with scale radius rs, scale density rhos, and cusp amplitude A.
+  profile with scale radius r_s, scale density rho_s, and cusp amplitude A.
   We assume an isotropic velocity distribution.
   
-  This profile only makes sense if rhos * rs**1.5 >= A.
+  This profile only makes sense if rho_s * r_s**1.5 >= A.
   
   If G is not specified, we return sigma_r/sqrt(G), which has dimensions of
   sqrt(mass/length).
   '''
   rmin = np.min(r)
-  rmax = 30.*max(np.max(r),rs)
+  rmax = 30.*max(np.max(r),r_s)
   Nr = int(np.round(np.log(rmax/rmin)/np.log(1.02))) # step in factors of 1.02
   _r = np.geomspace(rmin,rmax,Nr)
-  _p = density(_r,rs,rhos,A)
-  _m = mass(_r,rs,rhos,A)
-  _ps2_0 = 4*np.pi*G*rhos*rs**2*__sigma2_r_large(_r[-1]/rs) * _p[-1]
+  _p = density(_r,r_s,rho_s,A)
+  _m = mass(_r,r_s,rho_s,A)
+  _ps2_0 = 4*np.pi*G*rho_s*r_s**2*__sigma2_r_large(_r[-1]/r_s) * _p[-1]
   _ps2 = cumtrapz(-(_p*G*_m/_r)[::-1],x=np.log(_r)[::-1],initial=0)[::-1] + _ps2_0
   return np.interp(np.log(r),np.log(_r),np.sqrt(_ps2/_p),left=np.nan,right=np.nan)
