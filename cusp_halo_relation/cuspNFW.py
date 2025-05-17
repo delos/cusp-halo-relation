08e653c95a14f1fd00a229ef01fa946851a08d0d
Sten Delos <sten@mpa-garching.mpg.de>
2025-05-17 15:23:29 -0700

    add DF

--- cusp_halo_relation/cuspNFW.py
+++ cusp_halo_relation/cuspNFW.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.optimize import root_scalar
-from scipy.integrate import cumtrapz
+from scipy.integrate import cumtrapz, simpson
 
 # dimensionless radial profiles
 
@@ -14,34 +14,53 @@ def __mass_largey(y,x):
   return np.sqrt(x)*(-30 - 10*x*(7 - y) + x**2*(-37 + y*(4 + 3*y)))/(15.*(1 + x)**2.5) + 2*np.arcsinh(np.sqrt(x))
 def __mass(x,y):
   x,y = np.broadcast_arrays(x,y)
+  y = y.astype(float)
   return 4*np.pi*np.piecewise(y,[(0.<=y)&(y<0.001),(0.001<=y)&(y<=0.999),(0.999<y)&(y<1.001)],[__mass_smally,__mass_midy,__mass_largey,np.nan],x)
 def __veldisp2_r_NFW_largex(x):
   logx = np.log(x)
   return (-3./16+logx/4)/x + (69./200+logx/10)/x**2 + (-97./1200-logx/20)/x**3 + (71./3675+logx/35)/x**4 + (-1./3136-logx/56)/x**5 + (-1271./211680+logx/84)/x**6
 def __veldisp2_r(x,y):
   xmin, xmax = np.min(x), 30.*max(np.max(x),1.)
-  Nx = int(np.round(np.log(xmax/xmin)/np.log(1.02))) # step in factors of 1.02
+  Nx = 1+int(np.ceil(np.log(xmax/xmin)/np.log(1.02))) # step in factors of 1.02
   _x = np.geomspace(xmin,xmax,Nx)
   _p, _m = __density(_x,y), __mass(_x,y)
   _ps2_0 = 4*np.pi*__veldisp2_r_NFW_largex(_x[-1]) * _p[-1]
   _ps2 = cumtrapz(-(_p*_m/_x)[::-1],x=np.log(_x)[::-1],initial=0)[::-1] + _ps2_0
   return np.exp(np.interp(np.log(x),np.log(_x),np.log(_ps2/_p),left=np.nan,right=np.nan))
+def __potential_NFW_smallx(x):
+  return x/2. - x**2/3. + x**3/4. - x**4/5. + x**5/6. - x**6/7.
+def __potential_NFW_largex(x):
+  return 1. - np.log(1.+x)/x
+def __potential_NFW(x):
+  return 4*np.pi*np.piecewise(x,[x<=1e-3],[__potential_NFW_smallx,__potential_NFW_largex])
 def __potential_smallx(x,y):
   return np.pi/x * ((2*x-y**2)*np.sqrt(x*(x+y**2)) + (4*x+y**2)*y**2*np.log((np.sqrt(x)+np.sqrt(x+y**2))/y))
-def __potential(x,y):
-  x = np.array(x)
-  pot = np.zeros(np.shape(x))
-  small = x <= 1e-3
-  pot[small] = __potential_smallx(x[small],y)
-  xmin, xmax = 1e-3, np.max(x)
-  if xmax > xmin:
-    Nx = int(np.round(np.log(xmax/xmin)/np.log(1.02))) # step in factors of 1.02
-    _x = np.geomspace(xmin,xmax,Nx)
-    _m = __mass(_x,y)
-    _pot_0 = __potential_smallx(_x[0],y)
-    _pot = cumtrapz(_m/_x,x=np.log(_x),initial=0) + _pot_0
-    pot[~small] = np.exp(np.interp(np.log(x[~small]),np.log(_x),np.log(_pot),left=np.nan,right=np.nan))
+def __potential(x,y,zero_at_inf=False):
+  x = np.array(x).astype(float)
+  if y == 0.:
+    pot = __potential_NFW(x)
+  else:
+    pot = np.zeros(np.shape(x))
+    small = x <= 1e-3
+    pot[small&(x>0.)] = __potential_smallx(x[small&(x>0.)],y)
+    xmin, xmax = 1e-3, np.max(x)
+    if xmax > xmin:
+      Nx = 1+int(np.ceil(np.log(xmax/xmin)/np.log(1.02))) # step in factors of 1.02
+      _x = np.geomspace(xmin,xmax,Nx)
+      _m = __mass(_x,y)
+      _pot_0 = __potential_smallx(_x[0],y)
+      _pot = cumtrapz(_m/_x,x=np.log(_x),initial=0) + _pot_0
+      pot[~small] = np.exp(np.interp(np.log(x[~small]),np.log(_x),np.log(_pot),left=np.nan,right=np.nan))
+  if zero_at_inf:
+    return pot - __potential_inf(y)
   return pot
+def __potential_inf_midy(y):
+  return 1. - 0.5*y**2/np.sqrt(1.-y**2) * np.log(2.*(1.-np.sqrt(1.-y**2))/y**2 - 1.)
+def __potential_inf_largey(y):
+  return 2./3 * (1.+2*y)
+def __potential_inf(y):
+  y = np.array(y).astype(float)
+  return 4*np.pi*np.piecewise(y,[(0.<=y)&(y<0.0001),(0.0001<=y)&(y<=0.9999),(0.9999<y)&(y<1.0001)],[1.,__potential_inf_midy,__potential_inf_largey,np.nan])
 
 # radial profiles
 
@@ -89,18 +108,70 @@ def veldisp_r(r,r_s,rho_s,A,G=1.):
   '''
   return np.sqrt(veldisp2_r(r,r_s,rho_s,A,G=G))
 
-def potential(r,r_s,rho_s,A,G=1.):
+def potential(r,r_s,rho_s,A,G=1.,zero_at_inf=False):
   '''
-  Evaluate radial velocity dispersion at radius r for a cusp-NFW density
+  Evaluate gravitational potential Phi at radius r for a cusp-NFW density
   profile with scale radius r_s, scale density rho_s, and cusp coefficient A.
-  We assume an isotropic velocity distribution.
+  By default, we take the potential to be zero at r=0.
   
   This profile only makes sense if rho_s * r_s**1.5 >= A.
   
-  If G is not specified, we return sigma_r/sqrt(G), which has dimensions of
-  sqrt(mass/length).
+  If G is not specified, we return Phi/G, which has dimensions of mass/length.
   '''
-  return G*rho_s*r_s**2 * __potential(r/r_s,A/(rho_s*r_s**1.5))
+  return G*rho_s*r_s**2 * __potential(r/r_s,A/(rho_s*r_s**1.5),zero_at_inf=zero_at_inf)
+
+# distribution function
+
+def __df(e,y):
+  step = 1.01 # step in factors of 1.01
+  # get drho/dPhi
+  xmax = 1e5
+  xmin = 9./(256.*np.pi**2) * np.min(e)**2 / step**3
+  Nx = 1+int(np.ceil(np.log(xmax/xmin)/np.log(step)))
+  _x = np.geomspace(xmin,xmax,Nx)
+  _p = __density(_x,y)
+  _P = __potential(_x,y)
+  __P = np.sqrt(_P[1:]*_P[:-1])
+  __dpdP = np.sqrt(_p[1:]*_p[:-1]/(_P[1:]*_P[:-1])) * np.diff(np.log(_p))/np.diff(np.log(_P))
+  # choose range of energies
+  # (we log-space in difference from 0 and from inf, whichever is smaller)
+  emin, emax = np.min(e)/step, min(np.max(e)*step,__P[-1]/step)
+  emidL = 0.5*__potential_inf(y)/np.sqrt(step)
+  emidU = 0.5*__potential_inf(y)*np.sqrt(step)
+  if emin <= emidL:
+    NeL = 1+int(np.ceil(np.log(emidL/emin)/np.log(step)))
+    _eL = np.geomspace(emin,emidL,NeL)
+  else:
+    _eL = np.empty(0)
+  if emax > emidU:
+    NeU = 1+int(np.ceil(np.log((1-emidU/step/emax)/(1-1/step))/np.log(step)))
+    _eU = emax*step * (1.- np.geomspace(1-emidU/step/emax,1-1/step,NeU))
+  else:
+    _eU = np.empty(0)
+  _e = np.concatenate((_eL,_eU))
+  _fint = np.zeros_like(_e)
+  # evaluate DF
+  for i,e1 in enumerate(_e):
+    dpdP1 = -np.exp(np.interp(np.log(e1),np.log(__P),np.log(-__dpdP),left=np.nan,right=np.nan))
+    integrand = np.insert(__dpdP[__P>e1],0,dpdP1)
+    measure = np.insert(np.sqrt(__P[__P>=e1]-e1),0,0.)
+    _fint[i] = 1./(np.sqrt(2)*np.pi**2) * simpson(integrand,x=measure)
+  __e = np.sqrt(_e[1:]*_e[:-1])
+  __f = np.diff(_fint)/np.diff(_e)
+  return np.exp(np.interp(np.log(e),np.log(__e),np.log(__f),left=-np.inf,right=-np.inf))
+
+def df(E,r_s,rho_s,A,G=1.):
+  '''
+  Evaluate the distribution function f(E) for a cusp-NFW density profile wit
+  h scale radius r_s, scale density rho_s, and cusp coefficient A. We assume an
+  isotropic velocity distribution.
+  
+  This profile only makes sense if rho_s * r_s**1.5 >= A.
+  
+  If G is not specified, we assume the first argument is E/G, which has
+  dimensions of mass/length.
+  '''
+  return __df(E/(G*rho_s*r_s**2),A/(rho_s*r_s**1.5))
 
 # conversions from halo concentration
 
