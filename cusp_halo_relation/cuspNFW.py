8aefaa597524a1a1ec388a870db777ae08c39f08
Sten Delos <sten@mpa-garching.mpg.de>
2025-05-17 10:11:35 -0700

    add potential

--- cusp_halo_relation/cuspNFW.py
+++ cusp_halo_relation/cuspNFW.py
@@ -2,38 +2,111 @@ import numpy as np
 from scipy.optimize import root_scalar
 from scipy.integrate import cumtrapz
 
+# dimensionless radial profiles
+
+def __density(x,y):
+  return np.sqrt(x+y**2)/(x**1.5*(1+x)**2)
+def __mass_midy(y,x):
+  return 2*np.arcsinh(np.sqrt(x)/y) - (2 - y**2)*np.arctanh(np.sqrt(x*(1 - y**2)/(x + y**2)))/np.sqrt(1 - y**2) - np.sqrt(x*(x + y**2))/(1 + x)
+def __mass_smally(y,x):
+  return np.log(1+x) - x*(1-0.5*y**2)/(1.+x)
+def __mass_largey(y,x):
+  return np.sqrt(x)*(-30 - 10*x*(7 - y) + x**2*(-37 + y*(4 + 3*y)))/(15.*(1 + x)**2.5) + 2*np.arcsinh(np.sqrt(x))
+def __mass(x,y):
+  x,y = np.broadcast_arrays(x,y)
+  return 4*np.pi*np.piecewise(y,[(0.<=y)&(y<0.001),(0.001<=y)&(y<=0.999),(0.999<y)&(y<1.001)],[__mass_smally,__mass_midy,__mass_largey,np.nan],x)
+def __veldisp2_r_NFW_largex(x):
+  logx = np.log(x)
+  return (-3./16+logx/4)/x + (69./200+logx/10)/x**2 + (-97./1200-logx/20)/x**3 + (71./3675+logx/35)/x**4 + (-1./3136-logx/56)/x**5 + (-1271./211680+logx/84)/x**6
+def __veldisp2_r(x,y):
+  xmin, xmax = np.min(x), 30.*max(np.max(x),1.)
+  Nx = int(np.round(np.log(xmax/xmin)/np.log(1.02))) # step in factors of 1.02
+  _x = np.geomspace(xmin,xmax,Nx)
+  _p, _m = __density(_x,y), __mass(_x,y)
+  _ps2_0 = 4*np.pi*__veldisp2_r_NFW_largex(_x[-1]) * _p[-1]
+  _ps2 = cumtrapz(-(_p*_m/_x)[::-1],x=np.log(_x)[::-1],initial=0)[::-1] + _ps2_0
+  return np.exp(np.interp(np.log(x),np.log(_x),np.log(_ps2/_p),left=np.nan,right=np.nan))
+def __potential_smallx(x,y):
+  return np.pi/x * ((2*x-y**2)*np.sqrt(x*(x+y**2)) + (4*x+y**2)*y**2*np.log((np.sqrt(x)+np.sqrt(x+y**2))/y))
+def __potential(x,y):
+  pot = np.zeros(np.shape(x))
+  small = x<1e-3
+  pot[small] = __potential_smallx(x[small],y)
+  xmin, xmax = max(1e-3,np.min(x)), np.max(x)
+  if xmax > xmin:
+    Nx = int(np.round(np.log(xmax/xmin)/np.log(1.02))) # step in factors of 1.02
+    _x = np.geomspace(xmin,xmax,Nx)
+    _m = __mass(_x,y)
+    _pot_0 = __potential_smallx(_x[0],y)
+    _pot = cumtrapz(_m/_x,x=np.log(_x),initial=0) + _pot_0
+    pot[~small] = np.exp(np.interp(np.log(x[~small]),np.log(_x),np.log(_pot),left=np.nan,right=np.nan))
+  return pot
+
+# radial profiles
+
 def density(r,r_s,rho_s,A):
   '''
   Evaluate density at radius r for a cusp-NFW density profile with scale radius
-  r_s, scale density rho_s, and cusp amplitude A.
+  r_s, scale density rho_s, and cusp coefficient A.
   
   This profile only makes sense if rho_s * r_s**1.5 >= A.
   '''
-  x = r/r_s
-  y = A / (rho_s * r_s**1.5) # <= 1
-  return np.sqrt(x+y**2)/(x**1.5*(1+x)**2) * rho_s
-
-def __mass(y,x):
-  return 2*np.arcsinh(np.sqrt(x)/y) - (2 - y**2)*np.arctanh(np.sqrt(x*(1 - y**2)/(x + y**2)))/np.sqrt(1 - y**2) - np.sqrt(x*(x + y**2))/(1 + x)
-def __mass_smallA(y,x):
-  return np.log(1+x) - x*(1-0.5*y**2)/(1.+x)
-def __mass_largeA(y,x):
-  return np.sqrt(x)*(-30 - 10*x*(7 - y) + x**2*(-37 + y*(4 + 3*y)))/(15.*(1 + x)**2.5) + 2*np.arcsinh(np.sqrt(x))
+  return __density(r/r_s,A/(rho_s*r_s**1.5)) * rho_s
 
 def mass(r,r_s,rho_s,A):
   '''
   Evaluate mass enclosed within radius r for a cusp-NFW density profile with
-  scale radius r_s, scale density rho_s, and cusp amplitude A.
+  scale radius r_s, scale density rho_s, and cusp coefficient A.
   
   This profile only makes sense if rho_s * r_s**1.5 >= A.
   '''
-  x,y = np.broadcast_arrays(r/r_s,A/(rho_s * r_s**1.5))
-  return 4*np.pi*r_s**3*rho_s*np.piecewise(y,[(0.<=y)&(y<0.001),(0.001<=y)&(y<=0.999),(0.999<y)&(y<1.001)],[__mass_smallA,__mass,__mass_largeA,np.nan],x)
+  return r_s**3*rho_s * __mass(r/r_s,A/(rho_s*r_s**1.5))
+
+def veldisp2_r(r,r_s,rho_s,A,G=1.):
+  '''
+  Evaluate squared radial velocity dispersion, sigma_r^2, at radius r for a
+  cusp-NFW density profile with scale radius r_s, scale density rho_s, and cusp
+  coefficient A. We assume an isotropic velocity distribution.
+  
+  This profile only makes sense if rho_s * r_s**1.5 >= A.
+  
+  If G is not specified, we return sigma_r^2/G, which has dimensions of
+  mass/length.
+  '''
+  return G*rho_s*r_s**2 * __veldisp2_r(r/r_s,A/(rho_s*r_s**1.5))
+
+def veldisp_r(r,r_s,rho_s,A,G=1.):
+  '''
+  Evaluate radial velocity dispersion sigma_r at radius r for a cusp-NFW
+  density profile with scale radius r_s, scale density rho_s, and cusp
+  coefficient A. We assume an isotropic velocity distribution.
+  
+  This profile only makes sense if rho_s * r_s**1.5 >= A.
+  
+  If G is not specified, we return sigma_r/sqrt(G), which has dimensions of
+  sqrt(mass/length).
+  '''
+  return np.sqrt(veldisp2_r(r,r_s,rho_s,A,G=G))
+
+def potential(r,r_s,rho_s,A,G=1.):
+  '''
+  Evaluate radial velocity dispersion at radius r for a cusp-NFW density
+  profile with scale radius r_s, scale density rho_s, and cusp coefficient A.
+  We assume an isotropic velocity distribution.
+  
+  This profile only makes sense if rho_s * r_s**1.5 >= A.
+  
+  If G is not specified, we return sigma_r/sqrt(G), which has dimensions of
+  sqrt(mass/length).
+  '''
+  return G*rho_s*r_s**2 * __potential(r/r_s,A/(rho_s*r_s**1.5))
+
+# conversions from halo concentration
 
 def r2_from_rs(r_s,rho_s,A):
   '''
   Find the radius r_{-2} at which dlog(rho)/dlog(r) = -2 for a cusp-NFW density
-  profile with scale radius r_s, scale density rho_s, and cusp amplitude A.
+  profile with scale radius r_s, scale density rho_s, and cusp coefficient A.
   
   r_{-2} ranges from r_s/2 (if A=rho_s*r_s**1.5) to r_s (if A=0).
   '''
@@ -43,7 +116,7 @@ def r2_from_rs(r_s,rho_s,A):
 def rs_from_r2(r_2,rho_s,A):
   '''
   Find the cusp-NFW scale radius r_s, given r_2 (the radius r_{-2} at which
-  dlog(rho)/dlog(r) = -2), the scale density rho_s, and the cusp amplitude A.
+  dlog(rho)/dlog(r) = -2), the scale density rho_s, and the cusp coefficient A.
   '''
   z = A / (rho_s * r_2**1.5) # <= 2**1.5
   f = (1. + 18.*z**2 + 0.75*z*np.sqrt(72 + 564*z**2 + 6*z**4))**(1./3)
@@ -63,7 +136,6 @@ def __min_params_smallc(c):
 def __min_params(c):
   return np.piecewise(c,[c<0.001],[__min_params_smallc,__min_params_largec])
 
-
 def scale_from_c(c,M,A,rho_vir,cmin_error=True):
   '''
   
@@ -76,7 +148,7 @@ def scale_from_c(c,M,A,rho_vir,cmin_error=True):
       Halo parameters.
     
     A: float
-      Cusp amplitude parameter.
+      Cusp coefficient parameter.
     
     rho_vir: float
       The virial density (e.g., 200 times the cosmological mean value).
@@ -109,21 +181,21 @@ def scale_from_c(c,M,A,rho_vir,cmin_error=True):
 
 def A_max(c,M,rho_vir):
   '''
-  Maximum sensible cusp amplitude A in a halo of mass M and concentration c.
+  Maximum sensible cusp coefficient A in a halo of mass M and concentration c.
   Here rho_vir is the virial density.
   '''
   return np.sqrt(M*rho_vir/__min_params(c))
 
 def M_min(c,A,rho_vir):
   '''
-  Minimum sensible halo mass M given concentration c and cusp amplitude A.
+  Minimum sensible halo mass M given concentration c and cusp coefficient A.
   Here rho_vir is the virial density.
   '''
   return __min_params(c) * A**2 / rho_vir
 
 def c_min(M,A,rho_vir):
   '''
-  Minimum sensible concentration c given halo mass M and cusp amplitude A.
+  Minimum sensible concentration c given halo mass M and cusp coefficient A.
   Here rho_vir is the virial density.
   '''
   params = M*rho_vir/A**2
@@ -131,28 +203,3 @@ def c_min(M,A,rho_vir):
     return 0.
   lnc = root_scalar(lambda lnc: np.log(__min_params(np.exp(lnc))/params),x0=0.,x1=1.).root
   return np.exp(lnc)
-
-def __sigma2_r_large(x):
-  logx = np.log(x)
-  return (-3./16+logx/4)/x + (69./200+logx/10)/x**2 + (-97./1200-logx/20)/x**3 + (71./3675+logx/35)/x**4 + (-1./3136-logx/56)/x**5 + (-1271./211680+logx/84)/x**6
-
-def sigma_r(r,r_s,rho_s,A,G=1.):
-  '''
-  Evaluate radial velocity dispersion at radius r for a cusp-NFW density
-  profile with scale radius r_s, scale density rho_s, and cusp amplitude A.
-  We assume an isotropic velocity distribution.
-  
-  This profile only makes sense if rho_s * r_s**1.5 >= A.
-  
-  If G is not specified, we return sigma_r/sqrt(G), which has dimensions of
-  sqrt(mass/length).
-  '''
-  rmin = np.min(r)
-  rmax = 30.*max(np.max(r),r_s)
-  Nr = int(np.round(np.log(rmax/rmin)/np.log(1.02))) # step in factors of 1.02
-  _r = np.geomspace(rmin,rmax,Nr)
-  _p = density(_r,r_s,rho_s,A)
-  _m = mass(_r,r_s,rho_s,A)
-  _ps2_0 = 4*np.pi*G*rho_s*r_s**2*__sigma2_r_large(_r[-1]/r_s) * _p[-1]
-  _ps2 = cumtrapz(-(_p*G*_m/_r)[::-1],x=np.log(_r)[::-1],initial=0)[::-1] + _ps2_0
-  return np.interp(np.log(r),np.log(_r),np.sqrt(_ps2/_p),left=np.nan,right=np.nan)
