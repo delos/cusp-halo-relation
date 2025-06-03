8c405b1ce6a8e3ef87aa8d9745fdce4b48d7e300
Sten Delos <sten@mpa-garching.mpg.de>
2025-06-03 11:22:05 -0700

    add example

--- cusp_halo_relation/__init__.py
+++ cusp_halo_relation/__init__.py
@@ -3,5 +3,5 @@ from .cusp_halo_concordance import CuspHaloStandard, CuspHaloWDM
 from . import cuspNFW
 from .cutoffs import Cutoff
 from .thermal_history import ThermalHistory
-from .perturbations import Transfer_table, transfer_EisensteinHu
+from .perturbations import Transfer_table, transfer_EisensteinHu, growth_late
 from .concentration import concentration_L13_NFW
